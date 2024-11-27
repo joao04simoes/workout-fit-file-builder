0e227cf14314223c79fce49cdb76071b510c2974
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2024-11-27 23:57:07 +0000

    blocks to represent the steps

--- workout fit builder front/src/components/step.vue
+++ workout fit builder front/src/components/step.vue
@@ -1,64 +1,82 @@
 <template>
     <div>
-
         <button for="messageInput" class="label" @click="saveValue">Step</button>
         <button for="fetchData" class="label" @click="sendData">Send Data</button>
 
         <div>
-            <input id="min" v-model="ValueMin" placeholder="minutos" />
+            <input id="min" type="number" min="0" v-model="ValueMin" placeholder="Minutes" />
+            :<input id="seconds" type="number" min="0" max="59" v-model="ValueSeconds" placeholder="Seconds" />
         </div>
         <div>
-            <input id="zone" v-model="Valuezone" placeholder="zona" />
+            <label for="zoneDropdown">Select a Zone:</label>
+            <select v-model="Valuezone" id="zoneDropdown">
+                <option v-for="zone in zones" :key="zone" :value="zone">{{ zone }}</option>
+            </select>
+
         </div>
 
 
+        <div v-for="(value, index) in inter.Vmin" :key="index" class="rectangle" :style="{
+            width: `${inter.Vmin[index] / 100}px`,   // Width based on Vmin
+            height: `${inter.Vzone[index] * 10}px`, // Height based on Vzone
+            backgroundColor: 'red',
+        }">
+        </div>
 
-        <pre> {{ jsonData }}</pre>
+        <pre>{{ jsonData }}</pre>
     </div>
 </template>
 
 <script>
-
-
 export default {
     name: "Step",
     data() {
         return {
-            ValueMin: "",
-            Valuezone: "",
-            "inter": {
-                Vmin: [],
-                Vzones: [],
-            }
+            ValueMin: "0",      // Minutes input
+            Valuezone: "",     // Zone input
+            ValueSeconds: "0",
+            zones: [1, 2, 3, 4, 5],  // Seconds input
+            inter: {
+                Vmin: [],  // Array for rectangle widths (calculated from minutes & seconds)
+                Vzone: [], // Array for rectangle heights (from zone)
+            },
         };
     },
     computed: {
         jsonData() {
-            return JSON.stringify(this.inter, null, 2);
+            return JSON.stringify(this.inter, null, 2); // Format JSON data for display
         },
     },
     methods: {
         async sendData() {
-            const jsonData = JSON.stringify(this.inter)
+            // Send JSON data to the backend
+            const jsonData = JSON.stringify(this.inter);
             console.log("Sending data:", jsonData); // Debug output
-            // Example using fetch to send JSON
-            await fetch('http://127.0.0.1:5000/api/data', {
-                method: 'POST',
+            await fetch("http://127.0.0.1:5000/api/data", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
                 body: jsonData,
             });
         },
 
-
         saveValue() {
 
-            if (this.ValueMin.trim() !== "" && this.Valuezone.trim() !== "") {
-                this.inter.Vmin.push(this.ValueMin);
-                this.inter.Vzones.push(this.Valuezone);
+            if (this.ValueMin !== "" && this.Valuezone !== "") {
+                if (this.ValueSeconds == "") {
+                    this.ValueSeconds = 0
+                }
+
+                const totalSeconds = (parseInt(this.ValueMin, 10) * 1000) + (parseInt(this.ValueSeconds, 10) / 60 * 1000);
+
+
+                this.inter.Vmin.push(totalSeconds);
+                this.inter.Vzone.push(parseInt(this.Valuezone, 10));
+                // Clear input fields
                 this.ValueMin = "";
                 this.Valuezone = "";
+                this.ValueSeconds = "";
             }
         },
     },
@@ -66,6 +84,11 @@ export default {
 </script>
 
 <style scoped>
+.rectangle {
+    display: inline-block;
+
+}
+
 .label {
     font-weight: bold;
     margin-bottom: 10px;
