516615cb214be56c807f90b1d4ff0bfeec2f1f69
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2025-01-27 09:36:40 +0000

    power

--- back/App.py
+++ back/App.py
@@ -4,7 +4,7 @@ from builder import workoutBuilder
 from dataBase import GetFitFile_from_db, GetAllNameFiles
 import json
 import os
-from calculadorPotencia.main import Potencia
+from power.main import Potencia
 
 import tempfile
 
